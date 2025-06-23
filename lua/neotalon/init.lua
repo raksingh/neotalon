390dbb08941c70effd751949b627b21bafd82717
Rakesh Singh <rakesh.singh@absa.africa>
2025-06-23 15:28:27 +0200

    Remember the cursor position

--- lua/neotalon/init.lua
+++ lua/neotalon/init.lua
@@ -109,3 +109,15 @@ vim.diagnostic.config({
   signs = true,
   float = { border = "rounded" },
 })
+
+-- Remember cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.api.nvim_win_set_cursor(0, { mark[1], mark[2] })
+    end
+  end
+})
+
