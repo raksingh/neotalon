95f42be2dc1ba1dbe994aab0fa5193e733094897
Rakesh Singh <rakesh@sysras.com>
2025-08-30 13:06:45 +0200

    Disable Netrw if using Snacks' explorer

--- lua/neotalon/conf/snacks.lua
+++ lua/neotalon/conf/snacks.lua
@@ -1,9 +1,10 @@
 local M = {}
 
 function M.setup()
-    require("snacks").setup({
-        
-    })
+	vim.g.loaded_netrw = 1
+	vim.g.loaded_netrwPlugin = 1
+	vim.api.nvim_create_user_command("Ex", "lua Snacks.picker.explorer()", {})
+	require("snacks").setup({})
 end
 
 return M
