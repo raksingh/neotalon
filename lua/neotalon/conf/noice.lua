eb90420efddadececcbf2cb1ed79fe4fa8d4022b
Rakesh Singh <rakesh@sysras.com>
2025-09-03 21:37:20 +0200

    Added doc border

--- lua/neotalon/conf/noice.lua
+++ lua/neotalon/conf/noice.lua
@@ -6,38 +6,9 @@ function M.setup()
 			progress = {
 				enabled = true,
 			},
-			documentation = {
-				view = "hover",
-				opts = {
-					lang = "markdown",
-					replace = true,
-					render = "markdown",
-					format = { "{message}" },
-					position = { row = 2, col = 2 },
-					size = {
-						-- max_width = 0.8 * vim.api.nvim_win_get_width(0),
-						max_height = 15,
-					},
-				},
-				border = {
-					style = "rounded",
-				},
-				win_options = {
-					concealcursor = "n",
-					conceallevel = 3,
-					winhighlight = {
-						Normal = "CmpPmenu",
-						FloatBorder = "DiagnosticSignInfo",
-					},
-				},
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = false,
-				},
-				hover = {
-					enabled = true,
-				},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
 			},
 		},
 		presets = {
